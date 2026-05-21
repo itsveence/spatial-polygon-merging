57ff20c00690984ee31287d51551dbf8a563a425
Vincent <dvadiasor@gmail.com>
2026-05-21 11:29:45 +0100

    Refactor mask processing functions and add execution time decorator

--- preprocessing/image_processing.py
+++ preprocessing/image_processing.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import numpy as np
 import cv2
 import tifffile as tiff
+from typing import Union
 from utils.logger import logger
+from utils.helpers import time_it
 
 def tiff_to_png(tiff_path: Path, png_path: Path) -> None:
     """Converts TIFF images to PNG format."""
@@ -20,43 +22,56 @@ def tiff_to_png(tiff_path: Path, png_path: Path) -> None:
     except Exception as e:
         logger.error(f"Failed to convert {tiff_path} to PNG: {e}")
 
-def mask_to_txt(mask_path: Path, txt_path: Path, min_area: int = 20) -> None:
-    """Converts mask images to text format."""
-    try:
-        mask = tiff.imread(mask_path)
-        logger.debug(f"Loaded mask from {mask_path} with shape {mask.shape} and dtype {mask.dtype}")
-        if mask.ndim == 3:
-            mask = mask[:, :, 0]
+def mask_to_polygon(mask_path: Path, min_area: int = 20, normalize: bool = True) -> list[list[Union[float, int]]]:
+    """COnverts binary mask to list of polygons."""
+    mask = tiff.imread(mask_path)
+    logger.debug(f"Loaded mask from {mask_path} with shape {mask.shape} and dtype {mask.dtype}")
+    if mask.ndim == 3:
+        mask = mask[:, :, 0]
 
-        mask = ~mask # Invert mask: 0/False becomes 255/True, and vice versa
+    mask = ~mask # Invert mask: 0/False becomes 255/True, and vice versa
 
-        binary = mask.astype(np.uint8)
-        contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        logger.debug(f"Found {len(contours)} contours in {mask_path}: {contours}")
+    binary = mask.astype(np.uint8)
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    logger.debug(f"Found {len(contours)} contours in {mask_path}: {contours}")
 
-        h, w = binary.shape
-        lines = []
+    h, w = binary.shape
+    polygons = []
 
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area < min_area:
-                continue
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area < min_area:
+            continue
 
-            # Simplify contour
-            epsilon = 0.01 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
+        # Simplify contour
+        epsilon = 0.01 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
 
-            if len(approx) < 3:
-                continue
+        if len(approx) < 3:
+            continue
 
-            points = approx.reshape(-1, 2)
+        points = approx.reshape(-1, 2)
 
+        if normalize:
             # normalize x,y coordinates
-            normalized = []
-            for x, y in points:
-                normalized.extend([x / w, y / h])
+            normalized = (points/[w, h]).ravel().tolist()
             logger.debug(f"Contour with area {area} has {len(points)} points, normalized: {normalized}")
-            line = f"{0} " + " ".join(f"{p:.6f}" for p in normalized)
+            polygons.append(normalized)
+        else:
+            polygon = points.ravel().tolist()
+            logger.debug(f"Contour with area {area} has {len(points)} points, raw: {polygon}")
+            polygons.append(polygon)
+
+    return polygons
+
+@time_it
+def mask_to_txt(mask_path: Path, txt_path: Path, min_area: int = 20) -> None:
+    """Converts mask images to YOLO text format."""
+    try:
+        polygons = mask_to_polygon(mask_path, min_area=min_area, normalize=True)
+        lines = []
+        for poly in polygons:
+            line = f"{0} " + " ".join(f"{p:.6f}" for p in poly)
             lines.append(line)
         with open(txt_path, "w") as f:
             f.write("\n".join(lines))
@@ -65,13 +80,13 @@ def mask_to_txt(mask_path: Path, txt_path: Path, min_area: int = 20) -> None:
 
 if __name__ == "__main__":    # Example usage
     tiff_file = Path("data/test/image/0.tif")
-    output_dir = Path("bin/data/test/image")
+    output_dir = Path("bin/tmp/data/test/image")
     output_dir.mkdir(parents=True, exist_ok=True)
     png_dir = output_dir / (tiff_file.stem + ".png")
     logger.info(f"Converting {tiff_file} to png format at {png_dir}")
     tiff_to_png(tiff_file, png_dir)
 
-    tiff_label = Path("data/test/label/1.tif")
+    tiff_label = Path("data/test/label/2_10.tif")
     txt_dir = output_dir / (tiff_label.stem + ".txt")
     logger.info(f"Converting {tiff_label} to txt format at {txt_dir}")
     mask_to_txt(tiff_label, txt_dir)
---
+++ utils/helpers.py
@@ -0,0 +1,22 @@
+import time
+from datetime import datetime, timedelta
+from functools import wraps
+from utils.logger import logger
+
+
+def time_it(func):
+    """Decorator to measure execution time of a function."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed = end_time - start_time
+        time_elapsed = (datetime.min + timedelta(seconds=elapsed)).strftime("%H:%M:%S.%f")
+        logger.debug(
+            f"\033[32mExecution time for {func.__name__}(): {time_elapsed}\033[0m"
+        )
+        return result
+
+    return wrapper
